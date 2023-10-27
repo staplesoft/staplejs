3ec40d3b1760b12c8a192fb48d0d7f3bf0c0d0d9
lrww <144589169+lrww@users.noreply.github.com>
2023-10-27 20:13:53 +0100

    Add Array module

---
+++ src/array.js
@@ -0,0 +1,16 @@
+if (typeof S_LIB === "undefined") var S_LIB = {};
+
+S_LIB = class extends Array {
+	static dimensionsOf(...lengths) {
+		var arr = new Array(lengths[0] ?? 0), i = lengths[0];
+
+		if (lengths.length > 1) {
+			var args = lengths.slice(1);
+			while(i--) arr[lengths[0] - 1 - i] = this.dimensionsOf(...args);
+		}
+
+		return arr;
+	}
+}
+
+export var Array = S_LIB.Array;
